0fbbbf5a3ff62eb966981f8abd299769240620ed
plopilpy <plopilpy@gmail.com>
2022-08-06 21:58:53 -0400

    rjhidhbfjn w

--- banner.js
+++ banner.js
@@ -1,2 +1,4 @@
+$( "#banner" ).load(function() {
 $( "#banner" ).attr('src', `https://plopilpy.is-a.dev/banner/banner1.html`).delay(800);
 $( "#banner" ).attr('src', `https://plopilpy.is-a.dev/banner/banner2.html`).delay(800);
+});
