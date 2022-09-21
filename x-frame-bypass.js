d2cb81feae3eabc733ca5ca2194bcbb9e6b76a7a
plopilpy <plopilpy@gmail.com>
2022-09-21 14:21:26 -0400

    Update x-frame-bypass.js

--- x-frame-bypass.js
+++ x-frame-bypass.js
@@ -69,7 +69,7 @@ customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
 	}
 	fetchProxy (url, options, i) {
 		const proxies = (options || {}).proxies || [
-			'https://cors-anywhere.herokuapp.com/'
+			'https://yacdn.org/proxy/'
 		]
 		return fetch(proxies[i] + url, options).then(res => {
 			if (!res.ok)
